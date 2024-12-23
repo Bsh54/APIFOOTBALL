a45f414188ee8536796bcabaec43db70b9cd5102
Shadrak Bsh <142589989+Bsh54@users.noreply.github.com>
2024-12-23 06:44:29 -0800

    Add files via upload

---
+++ run_all.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.Popen(['python3', 'foot.py'])
+subprocess.Popen(['python3', 'classements.py'])
+subprocess.Popen(['python3', 'incidents.py'])
+subprocess.Popen(['python3', 'scores.py'])
